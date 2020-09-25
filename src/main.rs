30f74c63c3e8fb54d646cea14136d9cc4a0ed09a
Systool <systool@magisystem.xyz>
2020-09-25 16:00:13 +0200

    Made a basic "get menu" endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,47 @@
+mod model;
+use actix_web::{
+	Responder,
+	web
+};
+use sqlx::MySqlPool;
+use futures::stream::StreamExt;
+
+//TODO: move services in an api module
+#[actix_web::get("/auth")]
+async fn get_jwt() -> impl Responder {
+	"Hello, world"
+}
+
+#[actix_web::get("/menu")]
+async fn get_menu(db: web::Data<MySqlPool>) -> impl Responder {
+	let products = sqlx::query_as!(
+		model::Product,
+		"SELECT * FROM products"
+	).fetch(db.get_ref())
+	 .filter_map(
+		 |item| futures::future::ready(
+				match item {
+					Ok(item) => Some(format!("{:?}", item)),
+					Err(_) => None
+				}
+		 )
+	 ).collect::<Vec<String>>().await;
+	web::Json(products)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 	//Parse .env (should make this optional)
 	dotenv::dotenv().expect(".env not found or not parsable");
 	//Enable Logging
-	simple_logger::SimpleLogger::new().init().expect("Couldn't start logger");
+	simple_logger::SimpleLogger::new()
+    .with_level(log::LevelFilter::Debug)
+		.init()
+		.expect("Couldn't set logger");
 
-	let conn = sqlx::MySqlPool::connect(
-		&*std::env::var("DATABASE_URL").expect("environment variable DATABASE_URL not defined")
+	let conn = get_database(
+		&*std::env::var("DATABASE_URL")
+				.expect("Environment variable DATABASE_URL not set")
 	).await
 	 .expect("Couldn't connect to database");
 
@@ -15,7 +50,16 @@ async fn main() -> std::io::Result<()> {
 		App::new()
 				.data(conn.clone())
 				.wrap(actix_web::middleware::Logger::default())
+				.service(get_jwt)
+				.service(get_menu)
 	).bind("0.0.0.0:8080")?
 	 .run()
 	 .await
+}
+
+async fn get_database(db_url: &str)->Result<MySqlPool, sqlx::Error> {
+	let conn = MySqlPool::connect(db_url).await?;
+	/*Check last order list addition,
+		truncate if older than a day*/
+	Ok(conn)
 }
---
+++ src/model.rs
@@ -0,0 +1,11 @@
+#[derive(serde::Serialize)]
+pub struct Product {
+	pub id: u32,
+	pub name: String 
+}
+
+impl std::fmt::Debug for Product {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}\t{}", self.id, self.name)
+    }
+}
