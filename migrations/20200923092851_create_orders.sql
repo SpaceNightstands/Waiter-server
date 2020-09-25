123d5959653986b4bf28339e2dbc1b16585d3834
Systool <systool@magisystem.xyz>
2020-09-25 15:38:41 +0200

    Added day field, made owner an index

--- migrations/20200923092851_create_orders.sql
+++ migrations/20200923092851_create_orders.sql
@@ -1,6 +1,8 @@
 -- Add migration script here
 CREATE TABLE orders (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	day DATE NOT NULL DEFAULT CURDATE(),
 	owner TINYTEXT NOT NULL,
-	cart JSON DEFAULT "{}"
+	cart JSON DEFAULT "{}",
+	INDEX(owner)
 )
