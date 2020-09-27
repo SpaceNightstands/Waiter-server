e6e1e2bb913e6cd5a81d3eb112c5e7377bd83e82
Systool <systool@magisystem.xyz>
2020-09-27 18:41:18 +0200

    Added order in model

--- src/model.rs
+++ src/model.rs
@@ -15,6 +15,40 @@ pub struct Product {
 
 impl std::fmt::Debug for Product {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}\t{}", self.id, self.name)
+        write!(f, "Product{{\n\tid: {}\n\tkind: {}\n\tname: {}\n}}", self.id, self.kind, self.name)
     }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Order {
+	#[serde(default)]
+	pub id: u32,
+	#[serde(
+		serialize_with = "serialize_date",
+		deserialize_with = "deserialize_date"
+	)]
+	pub day: NaiveDate,
+	pub owner: String,
+	pub cart: types::Json<Vec<u32>>,
+}
+
+impl std::fmt::Debug for Order {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+      write!(
+				f, "Order{{\n\tid: {}\n\tday: {}\n\towner: {}\n\tcart: {:?}\n}}",
+				self.id, self.day, self.owner, self.cart
+			)
+    }
+}
+
+fn serialize_date<S: serde::Serializer>(day: &NaiveDate, ser: S) -> Result<S::Ok, S::Error> {
+	ser.serialize_i64(
+		day.and_hms(0, 0, 0)
+			 .timestamp()
+	)
+}
+
+fn deserialize_date<'de, D: serde::Deserializer<'de>>(de: D) -> Result<NaiveDate, D::Error> {
+	let ts = <i64 as serde::Deserialize>::deserialize(de)?;
+	Ok(chrono::NaiveDateTime::from_timestamp(ts, 0).date())
 }
