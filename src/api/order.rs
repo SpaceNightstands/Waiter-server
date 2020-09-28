1fed1221f19135c39f54dd3424afb31ee028aeb8
Systool <systool@magisystem.xyz>
2020-09-28 10:00:50 +0200

    Basic endpoints

--- src/api/order.rs
+++ src/api/order.rs
@@ -4,12 +4,14 @@ use model::Order;
 pub fn get_service() -> actix_web::Scope{
 	web::scope("/order")
     .route("", web::get().to(get_orders))
+    .route("", web::put().to(put_orders))
+    .route("/{id}", web::delete().to(delete_orders))
 }
 
 async fn get_orders(db: web::Data<MySqlPool>) -> impl Responder {
 	let orders = sqlx::query_as!(
 		Order,
-		"SELECT * FROM orders"
+		"SELECT id,owner,cart FROM orders"
 	).fetch(db.get_ref())
 	 .filter_map(
 		|item| futures::future::ready(result_ok_log(item))
@@ -17,5 +19,47 @@ async fn get_orders(db: web::Data<MySqlPool>) -> impl Responder {
 	web::Json(orders)
 }
 
-//TODO: add order
-//TODO: delete order
+async fn put_orders(db: web::Data<MySqlPool>, order: web::Json<Order>) -> Result<impl Responder, Error> {
+	log::debug!("Inserting Order");
+	let tx = db.get_ref()
+		.begin()
+		.await
+		.map_err(Error::new)?;
+	let orders = sqlx::query!(
+		"INSERT INTO orders(owner, cart) VALUES (?, ?) RETURNING id, owner, cart",
+		order.owner, order.cart
+	).fetch_one(db.get_ref())
+	 .await
+	 .map(make_order_from_row)
+	 .map_err(Error::new)?;
+	tx.commit().await.map_err(Error::new)?;
+	Ok(web::Json(orders))
+}
+
+async fn delete_orders(db: web::Data<MySqlPool>, web::Path(id): web::Path<u32>) -> Result<impl Responder, Error> {
+	log::debug!("Deleting Order {} from order list", id);
+	let tx = db.get_ref()
+		.begin()
+		.await
+		.map_err(Error::new)?;
+	let product = sqlx::query!(
+		"DELETE FROM orders WHERE id = ? RETURNING id, owner, cart",
+		id	
+	).fetch_one(db.get_ref())
+	 .await
+	 .map(make_order_from_row)
+	 .map_err(Error::new)?;
+	tx.commit().await.map_err(Error::new)?;
+	Ok(web::Json(product))
+}
+
+//Utils: 
+fn make_order_from_row(item: sqlx::mysql::MySqlRow) -> Order {
+	//To index into the row with get
+	use sqlx::prelude::Row;
+	Order{
+		id: item.get("id"),
+		owner: item.get("owner"),
+		cart: item.get("cart")
+	}
+}
--- src/model.rs
+++ src/model.rs
@@ -1,5 +1,4 @@
 use serde::{Serialize, Deserialize};
-use sqlx::types::chrono::{ self, NaiveDate };
 
 #[derive(Serialize, Deserialize)]
 pub struct Product {
@@ -23,11 +22,6 @@ impl std::fmt::Debug for Product {
 pub struct Order {
 	#[serde(default)]
 	pub id: u32,
-	#[serde(
-		serialize_with = "serialize_date",
-		deserialize_with = "deserialize_date"
-	)]
-	pub day: NaiveDate,
 	pub owner: String,
 	pub cart: String,
 }
@@ -35,20 +29,9 @@ pub struct Order {
 impl std::fmt::Debug for Order {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
       write!(
-				f, "Order{{\n\tid: {}\n\tday: {}\n\towner: {}\n\tcart: {:?}\n}}",
-				self.id, self.day, self.owner, self.cart
+				f, "Order{{\n\tid: {}\n\towner: {}\n\tcart: {:?}\n}}",
+				self.id, self.owner, self.cart
 			)
     }
 }
 
-fn serialize_date<S: serde::Serializer>(day: &NaiveDate, ser: S) -> Result<S::Ok, S::Error> {
-	ser.serialize_i64(
-		day.and_hms(0, 0, 0)
-			 .timestamp()
-	)
-}
-
-fn deserialize_date<'de, D: serde::Deserializer<'de>>(de: D) -> Result<NaiveDate, D::Error> {
-	let ts = <i64 as serde::Deserialize>::deserialize(de)?;
-	Ok(chrono::NaiveDateTime::from_timestamp(ts, 0).date())
-}
