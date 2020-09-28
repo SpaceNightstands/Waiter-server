dd999611a321d62f880ce854e7bea2be52141503
Systool <systool@magisystem.xyz>
2020-09-28 22:20:10 +0200

    [UNTESTED] Basic auth guard

--- src/api/auth.rs
+++ src/api/auth.rs
@@ -1,14 +1,54 @@
 use super::prelude::*;
 
-pub fn get_service() -> actix_web::Scope{
-	web::scope("/auth")
-    .route("", web::get().to(get_jwt))
+pub(super) struct AuthToken {
+	pub(super) account_id: String
 }
 
-/*TODO: Create and send back jwt
- *(or any other kind of authentication token)*/
-async fn get_jwt() -> impl Responder {
-	"Hello, world"
-}
+pub(super) fn jwt_guard(req: &actix_web::dev::RequestHead) -> bool {
+	use hmac::NewMac;
+	let key = hmac::Hmac::<sha2::Sha256>::new_varkey(b"Test")
+    .unwrap();
 
-//TODO: Create Route Guard
+	//Authorization: Bearer <claims>.<signature>
+	let auth_value = req.headers.get("Authorization")
+    .map(
+			|value| value.to_str()
+				.map(
+					|value| value.trim()
+						.strip_prefix("Bearer")
+				)
+		);
+	use log::error;
+	match auth_value {
+		Some(Ok(Some(token))) => {
+			use jwt::{VerifyWithKey, Error};
+			use std::collections::HashMap;
+			let claims: Result<HashMap<String, String>, Error> = token.verify_with_key(&key);
+			match claims {
+				Ok(claims) => {
+					if let Some(acc_id) = claims.remove("sub") {
+						req.extensions_mut().insert(
+							AuthToken {
+								account_id: acc_id
+							}
+						);
+						return true
+					} else {
+						error!("Can't find sub(ject) in jwt");
+					}
+				},
+				Err(error) => if log::log_enabled!(log::Level::Error){
+					error!("Can't read Authorization header: {}", error)
+				}
+			}
+		},
+		Some(Ok(None)) => error!("Can't read Bearer token"),
+		Some(Err(error)) => {
+			if log::log_enabled!(log::Level::Error){
+				error!("Can't read Authorization header: {}", error)
+			}
+		},
+		None => error!("Can't find Authorization header")
+	}
+	false
+}
