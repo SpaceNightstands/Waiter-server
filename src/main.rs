deb114e45c6fce535e00b38a863c4facda7e318d
Systool <systool@magisystem.xyz>
2020-10-01 00:22:27 +0200

    Made more configurable

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod model;
 mod api;
 mod error;
 
+use std::env::var as env_var;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 	//Parse .env (should make this optional)
@@ -13,19 +14,30 @@ async fn main() -> std::io::Result<()> {
 		.expect("Couldn't set logger");
 
 	let conn = get_database(
-		&*std::env::var("DATABASE_URL")
+		&*env_var("DATABASE_URL")
 				.expect("Environment variable DATABASE_URL not set")
 	).await
 	 .expect("Couldn't connect to database");
 
 	use actix_web::{HttpServer, App};
+	let folder = env_var("SERVER_DIRECTORY")
+    .map(
+			|dir| /*if !dir.starts_with("/") {
+			}*/
+			&*dir
+		).unwrap_or("/");
 	HttpServer::new(move ||
 		App::new()
 				.data(conn.clone())
 				.wrap(actix_web::middleware::Logger::default())
-				.service(api::get_service("/"))
-	).bind("0.0.0.0:8080")?
-	 .run()
+				.service(api::get_service(folder))
+	).bind(
+		format!(
+			"{}:{}",
+			env_var("SERVER_ADDRESS").map(|s| &*s).unwrap_or("0.0.0.0"),
+			env_var("SERVER_PORT").map(|s| &*s).unwrap_or("8080"),
+		)
+	)?.run()
 	 .await
 }
 
