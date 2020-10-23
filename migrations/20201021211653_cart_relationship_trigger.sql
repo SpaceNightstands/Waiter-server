e5d7de3071088021ecffc4a4ba9b322295397b96
Systool <systool@magisystem.xyz>
2020-10-23 16:13:42 +0200

    Enforcing max_num product field

---
+++ migrations/20201021211653_cart_relationship_trigger.sql
@@ -0,0 +1,9 @@
+-- Add migration script here
+CREATE TRIGGER cap_products
+	AFTER INSERT ON carts
+	FOR EACH ROW
+BEGIN
+	IF (SELECT SUM(carts.quantity) <= products.max_num FROM carts JOIN products ON carts.item = products.id WHERE carts.item = NEW.item) = 0 THEN
+		SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT='Too many items ordered';
+	END IF;
+END;
