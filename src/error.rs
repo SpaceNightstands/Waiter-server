36f20a159b36293f58bffc5f573ba43c62360808
Systool <systool@magisystem.xyz>
2021-01-21 19:11:15 +0100

    Moved EnumError

--- src/error.rs
+++ src/error.rs
@@ -107,13 +107,3 @@ impl ResponseError for Error {
     }
 }
 
-#[derive(Debug)]
-pub(crate) struct EnumError(pub(crate) &'static str);
-
-impl std::error::Error for EnumError {}
-
-impl Display for EnumError {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		f.write_str(self.0)
-	}
-}
--- src/model.rs
+++ src/model.rs
@@ -149,8 +149,20 @@ where
 			"available" => Ok(Available),
 			"orderable" => Ok(Orderable),
 			"beverage" => Ok(Beverage),
-			_ => Err(crate::error::EnumError("Invalid ENUM variant").into())
+			_ => Err(error::EnumError("Invalid ENUM variant").into())
 		}
 	}
 }
 
+mod error {
+	#[derive(Debug)]
+	pub(crate) struct EnumError(pub(crate) &'static str);
+
+	impl std::error::Error for EnumError {}
+
+	impl std::fmt::Display for EnumError {
+		fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+			f.write_str(self.0)
+		}
+	}
+}
