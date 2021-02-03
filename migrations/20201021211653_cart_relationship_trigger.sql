80b19ec5dd8807381f1ae2aab6a9e7b16476f59e
Systool <systool@magisystem.xyz>
2021-02-04 00:28:08 +0100

    Migrations fixed again

--- migrations/20201021211653_cart_relationship_trigger.sql
+++ migrations/20201021211653_cart_relationship_trigger.sql
@@ -1,5 +1,4 @@
 -- Add migration script here
-DELIMITER $$
 CREATE TRIGGER cap_products
 	AFTER INSERT ON carts
 	FOR EACH ROW
@@ -7,4 +6,4 @@ BEGIN
     IF (SELECT SUM(carts.quantity) <= products.max_num FROM carts JOIN products ON carts.item = products.id WHERE carts.item = NEW.item) = 0 THEN
         SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT='Too many items ordered';
     END IF;
-END$$
+END
