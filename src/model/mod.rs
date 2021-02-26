c2d1a22d46e6f14f5e3905598671be2ac26d81af
Systool <systool@magisystem.xyz>
2021-02-26 17:33:15 +0100

    Refactoring data model module

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -4,22 +4,21 @@ use std::{
 };
 use serde::{Serialize, Deserialize};
 use sqlx::{
-	Database,
-	Decode,
-	Encode,
-	Type
+	Type,
+	FromRow
 };
 use derive_getters::Getters;
 
-#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Type, Debug, Clone, Copy, PartialEq, Eq)]
 #[serde(rename_all="lowercase")]
+#[sqlx(rename_all = "lowercase")]
 pub enum ProductKind {
 	Available,
 	Orderable,
 	Beverage
 }
 
-#[derive(Serialize, Deserialize, Getters, sqlx::FromRow, Debug, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Getters, FromRow, Debug)]
 pub struct Product {
 	#[serde(default)]
 	pub(crate) id: u32,
@@ -70,7 +69,7 @@ mod image_from_base64 {
 	}
 }
 
-#[derive(Serialize, Deserialize, Getters, Debug, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Getters, Debug)]
 pub struct Order {
 	#[serde(default)]
 	pub(crate) id: u32,
@@ -79,71 +78,10 @@ pub struct Order {
 	pub(crate) cart: Vec<(u32, u32)>,
 }
 
-impl std::convert::Into<&'static str> for &ProductKind {
-	fn into(self) -> &'static str {
-		use ProductKind::*;
-		match self {
-			Available => "available",
-			Orderable => "orderable",
-			Beverage => "beverage"
-		}
-	}
-}
-
 impl Order {
 	pub(crate) fn cart_mut(&mut self) -> Pin<&mut Vec<(u32, u32)>> {
 		//TODO: make pin effective
 		Pin::new(&mut self.cart)
-	}
-}
-
-impl<DB> Type<DB> for ProductKind
-where
-	DB: Database,
-	&'static str: Type<DB>
-{
-	fn type_info() -> DB::TypeInfo {
-		<&'static str as Type<DB>>::type_info()
-	}
-
-	fn compatible(ty: &DB::TypeInfo) -> bool {
-		<&'static str as Type<DB>>::compatible(ty)
-	}
-}
-
-impl<'q, DB> Encode<'q, DB> for ProductKind
-where
-	DB: Database,
-	&'q str: Encode<'q, DB>
-{
-	fn encode_by_ref(&self, buf: &mut <DB as sqlx::database::HasArguments<'q>>::ArgumentBuffer) -> sqlx::encode::IsNull {
-		let string: &'static str = self.into();
-		string.encode(buf)
-	}
-
-	fn produces(&self) -> Option<DB::TypeInfo> {
-		<&'q str as Encode<'q, DB>>::produces(&self.into())
-	}
-
-	fn size_hint(&self) -> usize {
-		<&'q str as Encode<'q, DB>>::size_hint(&self.into())
-	}
-}
-
-impl<'r, DB> Decode<'r, DB> for ProductKind
-where
-	DB: Database,
-	&'r str: Decode<'r, DB>
-{
-	fn decode(value: <DB as sqlx::database::HasValueRef<'r>>::ValueRef) -> Result<Self, sqlx::error::BoxDynError> {
-		use ProductKind::*;
-		let kind = <&str as Decode<DB>>::decode(value)?;
-		match kind {
-			"available" => Ok(Available),
-			"orderable" => Ok(Orderable),
-			"beverage" => Ok(Beverage),
-			_ => Err(error::EnumError("Invalid ENUM variant").into())
-		}
 	}
 }
 
@@ -159,3 +97,6 @@ mod error {
 		}
 	}
 }
+
+#[cfg(test)]
+mod test;
