c0ede86fa56bd0f79ceb919131bef1c5b60e1995
Systool <systool@magisystem.xyz>
2021-02-26 19:46:35 +0100

    Added rudimentary build script

---
+++ build-qnap.sh
@@ -0,0 +1,13 @@
+#!/bin/env sh
+QNAP_TOOLCHAIN_PATH="/home/quq/x86_64-QNAP-linux-gnu"
+
+# Path to gcc
+LINKER_PATH="$QNAP_TOOLCHAIN_PATH/cross-tools/bin/x86_64-QNAP-linux-gnu-gcc"
+# Path to the lib folder
+LIB_PATH="$QNAP_TOOLCHAIN_PATH/fs/lib"
+
+export SQLX_OFFLINE=true
+export CC=$LINKER_PATH
+#Path to ar
+export AR="$QNAP_TOOLCHAIN_PATH/cross-tools/bin/x86_64-QNAP-linux-gnu-ar"
+cargo rustc "$@" -- "-Clinker=$LINKER_PATH" "-L $LIB_PATH"
