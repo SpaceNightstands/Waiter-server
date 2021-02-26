33b9887cd71931e650e4a058ddd41fe41319849c
Systool <systool@magisystem.xyz>
2021-02-26 17:33:53 +0100

    Started api module refactor and adapted queries to MariaDB 5

--- src/api/menu/mod.rs
+++ src/api/menu/mod.rs
@@ -6,16 +6,18 @@ use super::prelude::{
 pub(crate) fn get_service(filter: Option<filter::SubList>) -> actix_web::Scope{
 	let scope = web::scope("/menu")
     .route("", web::get().to(get_menu));
-	let protected_routes = web::scope("")
+
+	let protectable_routes = web::scope("")
 		.route("", web::put().to(put_menu))
 		.route("/{id}", web::delete().to(delete_menu));
+
 	if let Some(filter) = filter {
     scope.service(
-			protected_routes
+			protectable_routes
 				.wrap(filter::SubjectFilter(filter))
 		)
 	} else {
-		scope.service(protected_routes)
+		scope.service(protectable_routes)
 	}
 }
 
@@ -34,6 +36,7 @@ async fn get_menu(db: web::Data<MySqlPool>) -> Result<impl Responder, Error> {
 	let mut prod_stream = sqlx::query_as(
 		"SELECT * FROM products"
 	).fetch(&mut tx);
+
 	while let Some(prod) = prod_stream.next().await {
 		let prod: Product = prod?;
 		products.push(prod);
@@ -52,15 +55,20 @@ async fn put_menu(db: web::Data<MySqlPool>, prod: web::Json<Product>) -> Result<
 	let mut tx = db.get_ref()
 		.begin().await
 		.map_err(Error::from)?;
-	let product = sqlx::query!(
-		"INSERT INTO products(kind, name, price, max_num, ingredients, image) VALUES (?, ?, ?, ?, ?, ?) RETURNING *",
+
+	let insert_id = sqlx::query!(
+		"INSERT INTO products(kind, name, price, max_num, ingredients, image) VALUES (?, ?, ?, ?, ?, ?)",
 		prod.kind(), prod.name(),
 		prod.price(), prod.max_num(),
 		prod.ingredients(), prod.image()
-	).fetch_one(&mut tx).await
-	 .map(make_product_from_row)
-	 .map_err(Error::from)?;
+	).execute(&mut tx).await
+    .map_err(Error::from)?
+		.last_insert_id();
+
 	tx.commit().await.map_err(Error::from)?;
+
+	let mut product = prod.into_inner();
+	product.id = insert_id as u32;
 	Ok(web::Json(product))
 }
 
@@ -69,30 +77,27 @@ async fn delete_menu(db: web::Data<MySqlPool>, web::Path(id): web::Path<u32>) ->
 	let mut tx = db.get_ref()
 		.begin().await
 		.map_err(Error::from)?;
-	let product = sqlx::query!(
-		"DELETE FROM products WHERE id = ? RETURNING *",
+
+	let product = sqlx::query_as!(
+		Product,
+		"SELECT
+			id, kind as `kind: crate::model::ProductKind`,
+			name, price, max_num, ingredients, image
+		FROM products WHERE id = ?",
 		id	
 	).fetch_one(&mut tx).await
-	 .map(make_product_from_row)
 	 .map_err(Error::from)?;
+
+	sqlx::query!(
+		"DELETE FROM products WHERE id = ?",
+		id	
+	).execute(&mut tx).await
+	 .map_err(Error::from)?;
+
 	tx.commit().await.map_err(Error::from)?;
 	Ok(web::Json(product))
 }
 
 //TODO: edit product
-//Utils: 
-#[inline]
-fn make_product_from_row(item: sqlx::mysql::MySqlRow) -> Product {
-	//To index into the row with get
-	use sqlx::Row;
-	//TODO: use column names instead of positions
-	Product {
-		id: item.get(0),
-		kind: item.get(1),
-		name: item.get(2),
-		price: item.get(3),
-		max_num: item.get(4),
-		ingredients: item.get(5),
-		image: item.get(6)
-	}
-}
+#[cfg(test)]
+mod test;
---
+++ src/api/order/mod.rs
@@ -0,0 +1,155 @@
+use super::prelude::{
+	*,
+	model::Order
+};
+
+pub(crate) fn get_service(filter: Option<filter::SubList>) -> actix_web::Scope{
+	let scope = web::scope("/order")
+    .route("", web::get().to(get_orders))
+    .route("", web::put().to(put_orders));
+
+	let all_orders_service = web::resource("/all").route(web::get().to(get_all_orders));
+
+	if let Some(filter) = filter {
+		scope.service(all_orders_service.wrap(filter::SubjectFilter(filter)))
+	} else {
+		scope.service(all_orders_service)
+	}
+}
+
+//TODO: Turn into function/use stream adapters
+macro_rules! stream_to_vec {
+	($query: ident, $orders: ident) => {
+		//Iterate through all (id, owner, owner_name, cartItem[n].id, cartItem[n].quantity) tuples
+		while let Some(item) = $query.next().await {
+			//If the read throws, return the error
+			let item = item?;
+			//If the order array has at least one item, get a mutable reference
+			if let Some(order) = $orders.last_mut() {
+				/*If the last array element has the same id as the query,
+				*add the (cartItem[n].id, cartItem[n].quantity) tuple to the cart*/
+				if order.id() == &item.id {
+					order.cart_mut().push(
+						(item.item, item.quantity)
+					);
+					continue;
+				}
+			}
+			/*If the order array is empty or if the ids are different, create a
+			*new order object*/
+			$orders.push(
+				Order {
+					id: item.id,
+					owner: item.owner,
+					owner_name: item.owner_name,
+					cart: vec![(item.item, item.quantity)]
+				}
+			);
+		}
+	}
+}
+
+async fn get_orders(db: web::Data<MySqlPool>, req: web::HttpRequest) -> Result<impl Responder, Error> {
+	let owner = req.extensions();
+	let owner = get_auth_token(&owner)?;
+
+	let mut query = sqlx::query!(
+		"SELECT o.id,o.owner,o.owner_name,c.item,c.quantity
+		 FROM orders AS o INNER JOIN carts AS c
+		 ON o.id=c.order
+		 WHERE o.owner=?
+		 ORDER BY id",
+		owner.sub()
+	).fetch(db.get_ref());
+
+	let mut orders = Vec::<Order>::new();
+	stream_to_vec!(query, orders);
+	Ok(web::Json(orders))
+}
+
+async fn get_all_orders(db: web::Data<MySqlPool>) -> Result<impl Responder, Error> {
+	let mut query = sqlx::query!(
+		"SELECT o.id,o.owner,o.owner_name,c.item,c.quantity
+		 FROM orders AS o INNER JOIN carts AS c
+		 ON o.id=c.order
+		 ORDER BY id",
+	).fetch(db.get_ref());
+
+	let mut orders = Vec::<Order>::new();
+	stream_to_vec!(query, orders);
+	Ok(web::Json(orders))
+}
+
+#[derive(serde::Deserialize, serde::Serialize)]
+struct PutOrder {
+	owner_name: String,
+	//cart is an array of (item id, quantity) tuples
+	cart: Vec<(u32, u32)>
+}
+
+async fn put_orders(db: web::Data<MySqlPool>, put_order: web::Json<PutOrder>, req: web::HttpRequest) -> Result<impl Responder, Error> {
+	let owner = req.extensions();
+	let owner = get_auth_token(&owner)?;
+	if put_order.cart.len() <= 0 {
+		return Err(
+			Error::Static{
+				status: actix_web::http::StatusCode::BAD_REQUEST,
+				reason: "Request",
+				message: "The cart is empty"
+			}.into()
+		)
+	}
+
+	log::debug!("Inserting Order");
+	let mut tx = db.get_ref()
+		.begin()
+		.await
+		.map_err(Error::from)?;
+
+	let insert_id = sqlx::query!(
+		"INSERT INTO orders(owner, owner_name) VALUES (?, ?)",
+		owner.sub(), put_order.owner_name
+	).execute(&mut tx).await
+    .map_err(Error::from)?
+		.last_insert_id();
+
+	for (item, quantity) in put_order.cart.iter() {
+		sqlx::query!(
+			"INSERT INTO carts VALUES (?, ?, ?)",
+			insert_id, item, quantity
+		).execute(&mut tx)
+		 .await
+		 .map_err(Error::from)?;
+	}
+
+	tx.commit().await.map_err(Error::from)?;
+
+	let put_order = put_order.into_inner();
+	let order = Order {
+		id: insert_id as u32,
+		//TODO: Find a better way to pass the owner id
+		owner: owner.sub().clone(),
+		owner_name: put_order.owner_name,
+		cart: put_order.cart
+	};
+
+	Ok(web::Json(order))
+}
+
+//Utils: 
+#[inline]
+fn get_auth_token<'r>(req: &'r std::cell::Ref<'_, actix_web::dev::Extensions>) -> Result<&'r AuthToken, Error>{
+	req.get::<AuthToken>()
+    .ok_or(AuthError())
+}
+
+const fn AuthError() -> Error {
+	Error::Static{
+		status: actix_web::http::StatusCode::INTERNAL_SERVER_ERROR,
+		reason: "Authentication",
+		message: "JWT Token wasn't correctly validated"
+	}
+}
+
+#[cfg(test)]
+mod test;
