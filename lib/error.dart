bb6db1395aeaa9596c3357937aec88b27ea2f5be
quq <quq@magisystem.xyz>
2021-05-06 14:33:25 +0200

    Created Error

---
+++ lib/error.dart
@@ -0,0 +1,11 @@
+class Error {
+  final String reason;
+  final String message;
+
+  const Error(this.reason, this.message);
+
+  Map<String, String> toJson() => {
+    'reason': reason,
+    'message': message,
+  }
+}
